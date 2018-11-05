b79d2f9c641da45720464ff97e2de49df5e5ed8b
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-05 11:11:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+/*
+function findMatching(d, s) {
+  return d.filter(z=>z==s);
+}
+*/
+
+function findMatching(d, s) {
+  return d.filter(z=>z.toLowerCase() == s.toLowerCase())
+}
+
+
+function fuzzyMatch(d, s) {
+  return d.filter(z=>z.startsWith(s));
+}
+
+/*
+function fuzzyMatch(d, s) {
+  return d.filter(z=>z.toLowerCase().startsWith(s.toLowerCase()));
+}
+*/
+
+function matchName(d, s) {
+  return d.filter(z=>z.name==s)
+}
